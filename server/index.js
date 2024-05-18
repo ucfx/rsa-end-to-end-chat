36428304d588605e4672100be8cb797e9f29a551
ucfx <65844371+ucfx@users.noreply.github.com>
2024-05-18 01:23:36 +0100

    Create Socket server

--- server/index.js
+++ server/index.js
@@ -14,6 +14,23 @@ const usersMap = {};
 
 io.on("connection", async (socket) => {
   console.log("userConnected", socket.id);
+  const user = socket.handshake.auth.user;
+
+  if (user) {
+    usersMap[socket.id] = user;
+    socket.broadcast.emit("users", usersMap);
+  }
+
+  socket.on("sendMessage", (msg) => {
+    console.log("message", msg);
+    io.to(msg.to).emit("newMessage", msg.text);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("userDisconnected", socket.id);
+    delete usersMap[socket.id];
+    socket.broadcast.emit("users", usersMap);
+  });
 });
 
 server.listen(port, () => {
